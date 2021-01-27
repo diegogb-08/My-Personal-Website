f1c38ce4b7ba105b12e924d2507e1f55df9df5dd
Diego <d.garciabrisa@gmail.com>
2021-01-27 09:20:33 +0100

    Navbar Fixed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
+/* Hamburguer */
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
+})
+navbarLinks.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
 })
 
+
+/*Sticky*/
 
 window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
