20728d34dcdd4ee91d5091002107fbde84e0957e
Diego <d.garciabrisa@gmail.com>
2021-01-28 22:32:52 +0100

    Alert added to Submit button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 /* Hamburguer */
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
-})
+});
 
 navbarLinks.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
-})
+});
 
 
 /*Sticky*/
@@ -17,4 +17,14 @@ navbarLinks.addEventListener('click', () => {
 window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
-})
+});
+
+
+// Submit button
+
+function submit(){
+    alert("FBI has been reported! Thanks for your collaboration");
+}
+
+
+console.log("¿QUE ESTAS BUSCANDO DAVID?")
